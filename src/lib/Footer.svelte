38ce748f569f7e6b48899b65d18b56c28dfc4a40
FeliNUx42 <felix.bommier@gmail.com>
2023-01-05 17:35:38 +0100

    link target blank

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,4 @@
 <div class="absolute left-0 right-0 bottom-0 bg-gray-100 text-gray-300 text-center py-2">
-  <span class="font-medium mr-2">© 2023 Copyright:</span>  <a href="https://github.com/FeliNUx42" class="hover:underline">Félix Bommier</a>
+  <span class="font-medium mr-2">© 2023 Copyright:</span>
+  <a href="https://github.com/FeliNUx42" class="hover:underline" target="_blank">Félix Bommier</a>
 </div>
